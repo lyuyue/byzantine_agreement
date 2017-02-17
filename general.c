0e2ddc9b3d4767178ccaca0ce4fa447a9d2851d4
Yue Lyu <lyu.yue1992@gmail.com>
2017-02-16 21:10:17 -0500

    cmd debug info

--- general.c
+++ general.c
@@ -7,8 +7,7 @@
 
 struct sockaddr_in hostlist[MAX_HOSTS];
 int hostlist_len = 0;
-char *hostfile;
-char *order_txt;
+char *hostfile, *order_txt;
 int faulty = 0;
 int port = 0;
 int commander_id = LOCALHOST;
@@ -21,7 +20,8 @@ int order = -1;
 int value_set[2] = {0, 0};
 int multicast_list[MAX_HOSTS][MAX_HOSTS];
 pthread_t multicast_tid[MAX_HOSTS][MAX_HOSTS];
-uint32_t multicast_data[MAX_HOSTS];
+uint32_t multicast_data[MAX_HOSTS][MAX_HOSTS];
+int multicast_datalen[MAX_HOSTS];
 
 int stoi(char *data) {
     int result = 0;
@@ -36,12 +36,6 @@ int choice() {
     if (value_set[1] == 1 && value_set[0] == 0) {
         return 1;
     }
-    return 0;
-}
-
-int parse_byzantine(struct ByzantineMessage *data) {
-
-
     return 0;
 }
 
@@ -81,8 +75,9 @@ int main(int argc, char *argv[]) {
     // initialize
     bzeros((char *) &hostlist[0], sizeof(struct sockaddr_in) * MAX_HOSTS);
     bzeros((char *) &multicast_list[0][0], sizeof(int) * MAX_HOSTS * MAX_HOSTS);
-    bzeros((char *) &multicast_data[0], sizeof(uint32_t) * MAX_HOSTS));
+    bzeros((char *) &multicast_data[0][0], sizeof(uint32_t) * MAX_HOSTS * MAX_HOSTS);
     bzeros((char *) &multicast_tid[0][0], sizeof(pthread_t) * MAX_HOSTS * MAX_HOSTS);
+    bzeros((char *) &multicast_datalen[0], sizeof(int) * MAX_HOSTS);
 
     // parse arguments
 
@@ -148,19 +143,21 @@ int main(int argc, char *argv[]) {
     // } ByzantineMesage;
 
     if (commander_id == LOCALHOST) {
+        printf("This is Commander.\n");
         struct ByzantineMesage *msg = (struct ByzantineMesage *) malloc(sizeof(struct ByzantineMesage) + sizeof(uint32_t));
         msg->type = 1;
         msg->size = sizeof(struct ByzantineMesage) + sizeof(uint32_t);
-        msg->round = 0;
+        msg->round_n = 0;
         msg->order = order;
         uint32_t *ids = (uint32_t) ((struct ByzantineMesage *) msg + 1);
         *ids = commander_id;
 
         char ack_buf[BUF_SIZE];
 
-        for 
-            reliable_send(sockfd, &hostlist->sockaddr, (char *) msg, ack_buf);
-            list_itr = list_itr->next;
+        for (int i = 0; i < hostlist_len; i ++) {
+            if (self_id == i) continue;
+            printf("[BYZ_SEND] Round %d, send order %d to %d \n", msg->round_n, msg->order, i);
+            // TODO: send
         }
         return 0; 
     }
@@ -170,12 +167,6 @@ int main(int argc, char *argv[]) {
     int bytes_recv = 0;
     int commander_serverlen = sizeof(struct sockaddr_in);
 
-    // round 0: receiving v_0
-    bytes_recv = recvfrom(sockfd, recv_buf, BUF_SIZE, 0, (struct sockaddr *) commander_sockaddr, &commander_serverlen);
-    if (bytes_recv < 0) {
-        perror("ERROR recv v_0");
-        return 0;
-    }
     struct ByzantineMessage *msg = (struct ByzantineMessage *) recv_buf;
     uint32_t order_recv = msg->order;
     value_set[order_recv] = 1;
@@ -198,15 +189,18 @@ int main(int argc, char *argv[]) {
             int bytes_recv = recvfrom(sockfd, recv_buf, BUF_SIZE, 0, (struct sockaddr *) &hostlist[i], &serverlen);
             uint32_t *msg_type = (uint32_t *) recv_buf;
             if (*msg_type == 1) {
-                // TODO: send ACK;
-
                 // ByzantineMessage
                 struct ByzantineMessage *cur_msg = (struct ByzantineMessage *) recv_buf;
+
+                // TODO: send ACK;
+                printf("[ACK_SEND] Round %d, send ACK to %d\n", cur_msg->round_n, i);
+
                 // ignore out-of-data msg
                 if (cur_msg->round_n < round_n) continue;
                 // ignore existing order
                 if (value_set[cur_msg->order] == 1) continue;
 
+                printf("[BYZ_RECV] Round %d, receive order %d from "); 
                 value_set[cur_msg->order] = 1;
                 multicast_data[round_n + 1] = cur_msg->order;
                 uint32_t msg_size = cur_msg->size - (uint32_t) sizeof(struct ByzantineMessage);
@@ -215,8 +209,10 @@ int main(int argc, char *argv[]) {
                 cur_msg += 1;
                 uint32_t *itr = (uint32_t *) cur_msg;
                 for (int j = 0; j < ids_count; j++) {
+                    printf("%d ->", j);
                     multicast_list[round_n + 1][* (itr + j)] = 1;
                 }
+                printf("\n");
             } 
 
             if (*msg_type == 2) {
@@ -224,7 +220,7 @@ int main(int argc, char *argv[]) {
                 struct Ack *cur_ack = (struct Ack *) recv_buf;
                 uint32_t ack_round_n = cur_ack->round_n;
                 multicast_list[ack_round_n][i] = 0;
-
+                printf("[ACK_RECV] Round %d, receive ack from %d\n", ack_round_n, i);
             }
         }
 
